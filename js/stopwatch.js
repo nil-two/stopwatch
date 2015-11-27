af0318beaf4096983aa4553af39e44cc16f5b7e7
kusabashira <kusabashira227@gmail.com>
2015-11-27 23:51:53 +0900

    Use Date.now() to calculate time

--- js/stopwatch.js
+++ js/stopwatch.js
@@ -1,18 +1,21 @@
 (function() {
     var idle = {
         start: function() {
+            this.startTime = Date.now();
             this.intervalId = setInterval(function() {
-                this.set('time', this.get('time') + 0.01);
+                this.set('time', Date.now() - this.startTime + this.totalTime);
             }.bind(this), 10);
             this.state = running;
         },
         reset: function() {
             this.set('time', 0.0);
+	    this.totalTime = 0;
         }
     };
     var running = {
         stop: function() {
             clearInterval(this.intervalId);
+	    this.totalTime += Date.now() - this.startTime;
             this.state = idle;
         },
     };
@@ -20,6 +23,8 @@
     var Stopwatch = Ractive.extend({
         template: '#stopwatch_template',
         intervalId: -1,
+        startTime: 0,
+        totalTime: 0,
         state: idle,
 
         _buttonState: function(button) {
