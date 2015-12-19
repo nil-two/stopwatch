5f087f9d68920606d952a41d9106f656ca8e7b75
kusabashira <kusabashira227@gmail.com>
2015-12-19 21:54:36 +0900

    Adjust the indent to 2

--- js/stopwatch.js
+++ js/stopwatch.js
@@ -1,74 +1,74 @@
 (function() {
-    var idle = {
-        start: function() {
-            this.startTime = Date.now();
-            this.intervalId = setInterval(function() {
-                this.set('time', Date.now() - this.startTime + this.totalTime);
-            }.bind(this), 10);
-            this.state = running;
-        },
+  var idle = {
+    start: function() {
+      this.startTime = Date.now();
+      this.intervalId = setInterval(function() {
+        this.set('time', Date.now() - this.startTime + this.totalTime);
+      }.bind(this), 10);
+      this.state = running;
+    },
 
-        reset: function() {
-            this.set('time', 0.0);
-            this.totalTime = 0;
-        }
-    };
+    reset: function() {
+      this.set('time', 0.0);
+      this.totalTime = 0;
+    }
+  };
 
-    var running = {
-        stop: function() {
-            clearInterval(this.intervalId);
-            this.totalTime += Date.now() - this.startTime;
-            this.state = idle;
-        },
-    };
+  var running = {
+    stop: function() {
+      clearInterval(this.intervalId);
+      this.totalTime += Date.now() - this.startTime;
+      this.state = idle;
+    },
+  };
 
-    var Stopwatch = Ractive.extend({
-        template: '#stopwatch_template',
+  var Stopwatch = Ractive.extend({
+    template: '#stopwatch_template',
 
-        intervalId: -1,
-        startTime: 0,
-        totalTime: 0,
-        state: idle,
+    intervalId: -1,
+    startTime: 0,
+    totalTime: 0,
+    state: idle,
 
-        _buttonState: function(button) {
-            return (this.state[button] === undefined)
-                ? 'inactive'
-                : 'active';
-        },
+    _buttonState: function(button) {
+      return (this.state[button] === undefined)
+        ? 'inactive'
+        : 'active';
+    },
 
-        _updateButtonState: function() {
-            this.set('start', this._buttonState('start'));
-            this.set('stop', this._buttonState('stop'));
-            this.set('reset', this._buttonState('reset'));
-        },
+    _updateButtonState: function() {
+      this.set('start', this._buttonState('start'));
+      this.set('stop', this._buttonState('stop'));
+      this.set('reset', this._buttonState('reset'));
+    },
 
-        _click: function(button) {
-            if (this.state[button] === undefined)
-                return;
-            this.state[button].bind(this)();
-            this._updateButtonState();
-        },
+    _click: function(button) {
+      if (this.state[button] === undefined)
+        return;
+      this.state[button].bind(this)();
+      this._updateButtonState();
+    },
 
-        oninit: function() {
-            this.set('time', 0.00);
-            this._updateButtonState();
-        },
+    oninit: function() {
+      this.set('time', 0.00);
+      this._updateButtonState();
+    },
 
-        start: function() {
-            this._click('start');
-        },
+    start: function() {
+      this._click('start');
+    },
 
-        stop: function() {
-            this._click('stop');
-        },
+    stop: function() {
+      this._click('stop');
+    },
 
-        reset: function() {
-            this._click('reset');
-        },
-    });
+    reset: function() {
+      this._click('reset');
+    },
+  });
 
-    new Stopwatch({
-        el: '#stopwatch_app',
-        data: {},
-    });
+  new Stopwatch({
+    el: '#stopwatch_app',
+    data: {},
+  });
 })();
